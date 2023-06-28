d3c59a05f88b6f6d0cc430b969845fc1fe284245
Dok <pierredok@outlook.fr>
2023-06-28 16:52:44 +0200

    Training

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import axios from 'axios';
+import { useState } from 'react';
+import { Route, Routes, NavLink, Link } from 'react-router-dom';
+
+import CharacterDetails from "./components/characterDetails"
 
 function App() {
+
+  console.log("APP is invoked")
+
+
+  const [charactersArr, setCharactersArr] = useState(null);
+
+  useEffect(() => {
+    getCharactersFromApi()
+  }, []);
+    console.log("sending request to get the list of characters....")
+
+
+
+  const getCharactersFromApi =() => {
+    axios.get(`${process.env.REACT_APP_API_URL}/characters`)
+      .then( response => {
+        setCharactersArr(response.data);
+      })
+      .catch( e => console.log(e))
+    }
+
+
+  const renderListOfCharacters = () => {
+    if(charactersArr === null){
+      return <p>loading....</p>;
+    } else {
+      return charactersArr.map((characterObj) => {
+        return (
+          <div key={characterObj.id} className="character box">
+            Name: {characterObj.name} <br />
+            Weapon: {characterObj.weapon} <br />
+            <Link to={`/characters/${characterObj.id}`}>More details</Link>
+          </div>
+        )
+      })
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Charates App</h1>
+
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+      </nav>
+
+      
+
+      <Routes>
+        <Route path='/' element={renderListOfCharacters()} />
+        <Route path='/contact' element={<p>Display Contact page</p>} />
+        <Route path="/characters/:characterId"  element={<CharacterDetails callbackToUpdateList={getCharactersFromApi}/>} />
+      </Routes>
+
+
     </div>
   );
 }
